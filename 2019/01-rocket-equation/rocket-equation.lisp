ad860529074e5f4fc7700f32664debb47ff175b9
Ray Perry <rmperry09@gmail.com>
2019-12-01 09:46:31 -0600

    Common Lisp | Cleanup.

--- 2019/01-rocket-equation/rocket-equation.lisp
+++ 2019/01-rocket-equation/rocket-equation.lisp
@@ -2,20 +2,24 @@
   "Given a NUMBER, divide by 3, round down, and subtract 2."
   (- (floor (/ number 3)) 2))
 
+(defun calculate-fuel-with-fuel (parsed-number)
+  "Given a NUMBER, calculate the fuel requirement while considering the previous fuel requirement."
+  (loop
+    with next-value = (calculate-fuel (calculate-fuel parsed-number))
+    until (or (zerop next-value) (minusp next-value))
+    sum next-value into intermediate-value
+    do (setq next-value (calculate-fuel next-value))
+    finally (return (+ (calculate-fuel parsed-number) intermediate-value))))
+
 (defun main (&optional (list-of-numbers (map 'list 'parse-integer (uiop:read-file-lines "input.txt"))))
   (loop
     for parsed-number in list-of-numbers
-    sum (let* ((intermediate-value (calculate-fuel parsed-number))
-               (next-value (calculate-fuel intermediate-value)))
-          (loop
-            until (or (zerop next-value) (minusp next-value))
-            do (progn
-                  (setq intermediate-value (+ intermediate-value next-value))
-                 (setq next-value (calculate-fuel next-value))))
-          intermediate-value)))
+    sum (calculate-fuel-with-fuel parsed-number)))
 
 (defun run-tests ()
   "Run a suite of test cases."
   (assert (= (main '(14)) 2))
   (assert (= (main '(1969)) 966))
   (assert (= (main '(100756)) 50346)))
+
+
